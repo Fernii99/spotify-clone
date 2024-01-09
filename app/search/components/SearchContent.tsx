060fc7ffb1f1771e5d16a50d82d26e3fc19a06e6
asier Fernandez <asierfernandez@asiers-MacBook-Pro.local>
2024-01-09 19:55:17 +0100

    [ADD] Liked songs list Functionality added

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -3,6 +3,7 @@
 import { Song } from "@/types"
 
 import MediaItem from "@/components/MediaItem"
+import LikeButton from "@/components/LikedButton"
 
 interface SearchContentProps {
     songs: Song[]
@@ -12,19 +13,23 @@ const SearchContent: React.FC<SearchContentProps> = ({songs}) => {
 
     if(songs.length === 0){
         return( 
-        <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-            No songs found!
-        </div>)
+            <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+                No songs found!
+            </div>
+        )
     }
 
     return (
         <div className="flex flex-col gap-y-2 w-full px-6">
             {songs.map((song) => (
-                <div key={song.id} className="flex itemms-center gap-x-4 w-full">
+                <div 
+                key={song.id} 
+                className="flex items-center gap-x-4 w-full"
+                >
                     <div className="flex-1">
                         <MediaItem onClick={() => {}} data={song}/>
                     </div>
-                    {/* TODO: Add Like button here */}
+                    <LikeButton songId={song.id} />
                 </div>
             ))}
         </div>
